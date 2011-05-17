34110a822f623b46ad0579b0dec22c1958cf5cf0
Alexander Baldwin <zanders@zafr.net>
2011-05-17 11:07:43 +0100

    Complete rewrite of functionality to work cross-browser. Now works in FF 3.6+, Chrome, Safari 5, Opera 10+, IE 6+. Also updated minified version to reflect changes.

--- inputReplace.jquery.js
+++ inputReplace.jquery.js
@@ -16,26 +16,38 @@
 	$.extend($.fn, {
 		'inputReplace': function() {
 			var inputs = $(this);
+			// Filter out any elements that are not radio or checkbox inputs.
 			inputs = inputs.filter("input[type=radio], input[type=checkbox]");
 			$.each(inputs, function(index, element) {
-				var input = $(element),
-					type = input.attr("type"),
-					onClass = type + "on",
-					replace = $(document.createElement("span"));
-				// Replace the form element with the stylable element.
+				var input	= $(element),
+					type	= input.attr("type"),
+					onClass	= type + "on",
+					replace	= $(document.createElement("span"));
+				// Replace the original inputs with their stylable counterparts.
+				replace.addClass(type).insertAfter(input);
+				input.hide();
+				// When you click on the stylable element, trigger the click
+				// event for the respective original input, and the change event
+				// for all inputs with the same name.
 				replace.bind("click", function(event) {
+					// Triggering the click event will change the value of the
+					// original form input.
 					input.trigger("click");
-				}).addClass(type).insertAfter(input);
+					// Triggering the change event will the script to detect
+					// when to update the stylable element's visual
+					// representation of the original input.
+					$("input[name='" + input.attr("name") + "']").trigger("change");
+				});
+				// When the change event is triggered, update the stylable
+				// element's visual representation of the original form input.
+				input.bind("change", function(event) {
+					input.is(":checked") ? replace.addClass(onClass) : replace.removeClass(onClass);
+				});
+				// Make sure the visual representation is correct at the start
+				// of the script execution.
 				if(input.is(":checked")) {
 					replace.addClass(onClass);
 				}
-				input.bind("change", function(event) {
-					if(type == 'radio' && typeof event.srcElement !== "undefined") {
-						var others = $("input[name='" + input.attr("name") + "']");
-						others.trigger("change");
-					}
-					input.is(":checked") ? replace.addClass(onClass) : replace.removeClass(onClass);
-				}).hide();
 			});
 		}
 	});
